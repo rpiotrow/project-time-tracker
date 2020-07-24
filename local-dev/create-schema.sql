b32eff58f48e68472689b163d1a8961c2eefec6a
Rafal Piotrowski <rafal.piotrowski@protonmail.com>
2020-07-24 21:21:50 +0200

    Add names for check constraints in SQL schema.

--- local-dev/create-schema.sql
+++ local-dev/create-schema.sql
@@ -7,12 +7,13 @@ CREATE SCHEMA IF NOT EXISTS ptt_read_model;
 
 CREATE TABLE IF NOT EXISTS ptt_read_model.projects(
   db_id SERIAL PRIMARY KEY,
-  deleted_at TIMESTAMP CHECK (deleted_at >= updated_at),
+  deleted_at TIMESTAMP CONSTRAINT deleted_not_before_updated CHECK (deleted_at >= updated_at),
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL CHECK (updated_at >= created_at),
-  id VARCHAR UNIQUE CHECK(id != ''),
+  updated_at TIMESTAMP NOT NULL CONSTRAINT updated_not_before_created CHECK (updated_at >= created_at),
+  id VARCHAR UNIQUE CONSTRAINT id_not_empty CHECK(id != ''),
   owner UUID NOT NULL,
-  duration_sum BIGINT NOT NULL CHECK(duration_sum >= 0)
+  duration_sum INTERVAL NOT NULL
+    CONSTRAINT duration_sum_positive CHECK(duration_sum >= interval '0 seconds')
 );
 
 CREATE TABLE IF NOT EXISTS ptt_read_model.tasks(
@@ -21,20 +22,23 @@ CREATE TABLE IF NOT EXISTS ptt_read_model.tasks(
   project_id BIGINT NOT NULL REFERENCES ptt_read_model.projects(db_id),
   owner UUID NOT NULL,
   started_at TIMESTAMP NOT NULL,
-  duration BIGINT NOT NULL CHECK (duration >= 0),
-  volume INT CHECK (volume >= 0),
+  duration INTERVAL NOT NULL CONSTRAINT duration_positive CHECK (duration >= interval '0 seconds'),
+  volume INT CONSTRAINT volume_positive CHECK (volume >= 0),
   comment TEXT
 );
 
 CREATE TABLE IF NOT EXISTS ptt_read_model.statistics(
   db_id SERIAL PRIMARY KEY,
   owner UUID NOT NULL,
-  year SMALLINT NOT NULL CHECK (year >= 2020),
-  month SMALLINT NOT NULL CHECK (month >= 1 AND month <= 12),
-  number_of_tasks INT NOT NULL CHECK (number_of_tasks >= 0),
-  average_task_duration DECIMAL NOT NULL CHECK (average_task_duration >= 0),
-  average_task_volume DECIMAL CHECK (average_task_volume >= 0),
-  volume_weighted_average_task_duration DECIMAL CHECK (volume_weighted_average_task_duration >= 0),
+  year SMALLINT NOT NULL CONSTRAINT year_after_2020 CHECK (year >= 2020),
+  month SMALLINT NOT NULL CONSTRAINT month_in_range_1_to_12 CHECK (month >= 1 AND month <= 12),
+  number_of_tasks INT NOT NULL CONSTRAINT number_of_tasks_positive CHECK (number_of_tasks >= 0),
+  average_task_duration INTERVAL NOT NULL
+    CONSTRAINT average_task_duration_positive CHECK (average_task_duration >= interval '0 seconds'),
+  average_task_volume DECIMAL CONSTRAINT average_task_volume_positive CHECK (average_task_volume >= 0),
+  volume_weighted_average_task_duration INTERVAL
+    CONSTRAINT volume_weighted_average_task_duration_positive
+    CHECK (volume_weighted_average_task_duration >= interval '0 seconds'),
 
   UNIQUE(owner, year, month)
 );
