00560efeab06089f8e5d6dd6312d4341701abf91
Rafal Piotrowski <rafal.piotrowski@protonmail.com>
2020-08-27 23:35:45 +0200

    404 message in JSON format

--- read-side/src/main/scala/io/github/rpiotrow/ptt/read/web/Server.scala
+++ read-side/src/main/scala/io/github/rpiotrow/ptt/read/web/Server.scala
@@ -1,15 +1,17 @@
 package io.github.rpiotrow.ptt.read.web
 
 import fs2.Stream
+import fs2.text.utf8Encode
 import io.github.rpiotrow.ptt.read.configuration.WebConfiguration
-import org.http4s.implicits._
+import org.http4s.headers.`Content-Type`
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
+import org.http4s.{Headers, MediaType, Response, Status}
+import zio._
 import zio.clock.Clock
+import zio.config._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
-import zio._
-import zio.config._
 
 object Server {
   trait Service {
@@ -21,9 +23,15 @@ object Server {
   val live: ZLayer[Routes with ZConfig[WebConfiguration], Throwable, Server] =
     ZLayer.fromServices[Routes.Service, WebConfiguration, Server.Service] { (routes, configuration) =>
       new Service() {
-        override def stream: RIO[Clock, Stream[Task, Nothing]] =
+        override def stream: RIO[Clock, Stream[Task, Nothing]] = {
+          val jsonNotFound: Response[Task] =
+            Response(
+              Status.NotFound,
+              body = Stream("""{"error": "Not found"}""").through(utf8Encode),
+              headers = Headers(`Content-Type`(MediaType.application.json) :: Nil)
+            )
           ZIO.runtime[Clock].flatMap { implicit runtime =>
-            val httpApp            = routes.readSideRoutes().orNotFound
+            val httpApp            = routes.readSideRoutes().mapF(_.getOrElse(jsonNotFound))
             val httpAppWithLogging = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
             ZIO.descriptor.map { d =>
               val server = BlazeServerBuilder[Task](d.executor.asEC)
@@ -33,6 +41,7 @@ object Server {
               server.drain
             }
           }
+        }
       }
     }
 }
--- write-side/src/main/scala/io/github/rpiotrow/ptt/write/web/Server.scala
+++ write-side/src/main/scala/io/github/rpiotrow/ptt/write/web/Server.scala
@@ -5,20 +5,28 @@ import java.util.concurrent.Executors
 import cats.Monad
 import cats.effect._
 import fs2.Stream
+import fs2.text.utf8Encode
 import io.github.rpiotrow.ptt.write.configuration.AppConfiguration
-import org.http4s.implicits._
+import org.http4s.headers.`Content-Type`
 import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.{Headers, MediaType, Response, Status}
 
 import scala.concurrent.ExecutionContext
 
 object Server {
 
   def stream[F[_]: Monad: ContextShift: ConcurrentEffect: Timer](): Resource[F, Stream[F, Nothing]] = {
-    val webConfiguration = AppConfiguration.live.webConfiguration
+    val webConfiguration          = AppConfiguration.live.webConfiguration
+    val jsonNotFound: Response[F] =
+      Response(
+        Status.NotFound,
+        body = Stream("""{"error": "Not found"}""").through(utf8Encode),
+        headers = Headers(`Content-Type`(MediaType.application.json) :: Nil)
+      )
     Routes
       .live[F]()
       .map(routes => {
-        val httpApp        = routes.writeSideRoutes().orNotFound
+        val httpApp        = routes.writeSideRoutes().mapF(_.getOrElse(jsonNotFound))
         val loggingHttpApp = org.http4s.server.middleware.Logger.httpApp(true, true)(httpApp)
 
         val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(webConfiguration.threadPoolSize))
